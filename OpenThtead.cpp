4b66406f7c1cde0d4ab0c0de3031f86c0685ed8a
RoyLeibo <42297127+RoyLeibo@users.noreply.github.com>
2019-01-07 16:29:48 +0200

    Update OpenThtead.cpp

--- OpenThtead.cpp
+++ OpenThtead.cpp
@@ -2,4 +2,28 @@
 // Created by einat on 1/6/19.
 //
 
-#include "OpenThtead.h"
+#include "OpenThread.h"
+
+void* run_solver(void* arg) {
+  struct solver_struct* arg_struct_p = (solver_struct*)arg ;
+  struct solver_struct arg_struct = *arg_struct_p;
+  delete arg_struct_p;
+  
+  pthread_exit(0) ;
+  
+}
+
+void OpenThread::open_thread(int new_sock_fd) {
+
+    // after connection has established, initialize a thread for reading data from the simulator
+    pthread_t tid ;
+
+    // initialize struct
+    struct solver_struct* s_s = new solver_struct;
+    s_s->new_sock_fd = new_sock_fd ;
+    
+
+    // create a thread with the struct and a function which reads data from the simulator,
+    // parse it and update the variables map
+    pthread_create(&tid, nullptr, run_solver, r_s) ;
+}
