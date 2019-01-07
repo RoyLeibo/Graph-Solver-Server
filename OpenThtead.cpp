349399dd2b88c881e06cb2e23b142727ea3ede0f
RoyLeibo <42297127+RoyLeibo@users.noreply.github.com>
2019-01-07 23:18:11 +0200

    Update OpenThtead.cpp

--- OpenThtead.cpp
+++ OpenThtead.cpp
@@ -8,14 +8,18 @@ void* run_solver(void* arg) {
   struct solver_struct* arg_struct_p = (solver_struct*)arg ;
   struct solver_struct arg_struct = *arg_struct_p;
   delete arg_struct_p;
-  
-  
-  
+  int sock_fd ;
+  while(true) {
+    sock_fd = OpenSocket().open_socket(arg_struct.port) ;
+    arg_struct.c_h.handle_client(sock_fd) ;
+    
+      
+  }
   pthread_exit(0) ;
   
 }
 
-void OpenThread::open_thread(int port) {
+void OpenThread::open_thread(int port, ChlientHandler c_h) {
 
     // after connection has established, initialize a thread for reading data from the simulator
     pthread_t tid ;
@@ -23,7 +27,8 @@ void OpenThread::open_thread(int port) {
     // initialize struct
     struct solver_struct* s_s = new solver_struct;
     s_s->port = port ;
-    
+    s_s->c_h = c_h ;  
+  
     // create a thread with the struct and a function which reads data from the simulator,
     // parse it and update the variables map
     pthread_create(&tid, nullptr, run_solver, r_s) ;
