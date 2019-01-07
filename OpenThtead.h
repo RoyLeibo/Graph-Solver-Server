05ca211362cb6c86554c32a0f09c35314b883fba
RoyLeibo <42297127+RoyLeibo@users.noreply.github.com>
2019-01-07 16:29:45 +0200

    Update OpenThtead.h

--- OpenThtead.h
+++ OpenThtead.h
@@ -5,8 +5,16 @@
 #ifndef FINAL_PROJECT_SECOND_OPENTHTEAD_H
 #define FINAL_PROJECT_SECOND_OPENTHTEAD_H
 
+void* run_solver(void* arg) ;
 
-class OpenThtead {
+class OpenThread {
+
+  void open_thread() ;
+  
+};
+
+struct solver_struct {
+    int new_sock_fd ;
 
 };
 
